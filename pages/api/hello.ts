7883edc2a6237008dd42cc6c10efe7d927723628
Gabriel <68795815+Gabrielbzl001@users.noreply.github.com>
2022-11-29 21:01:52 -0300

    trying to resolve build problem

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,14 +9,6 @@ export const config = {
   runtime: 'experimental-edge',
 }
 
-export default async function (req: NextApiRequest) {
-  return new Response(
-    JSON.stringify({ name: 'John Doe' }),
-    {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }
-  )
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  res.status(200).json({name: 'John Teste'})
 }
