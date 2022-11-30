1bbcbc624a454bd41ea1f9453042f764d0e69724
Gabrielbzl001 <gbragazlima001@gmail.com>
2022-11-29 21:08:02 -0300

    export async function

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,14 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
-
 export const config = {
   runtime: 'experimental-edge',
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   res.status(200).json({name: 'John Teste'})
+  
+}
+
+export default async function (req: any) {
+  return new Response(
+    JSON.stringify({ name: 'John Doe' }),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  )
 }
